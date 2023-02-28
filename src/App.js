a907ee6f54d26fd896ff1ab8e69576122f680f4d
rozexcode <patrykrozpedek02@gmail.com>
2023-02-28 16:05:33 +0100

    2nd day of react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ExpenseRender from "./components/ExpenseRender.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello world</h2>
+      <p>siema</p>
+      <div>
+        <h1>Excersise done</h1>
+        <ExpenseRender />
+      </div>
+    </div>
+  );
+}
+
+export default App;
